6595b9f54011d636ba3682023a8c2bf052bf4c62
Oscar Moll <orm@csail.mit.edu>
2018-09-21 10:02:43 -0400

    mods

--- get-turns/osm.py
+++ get-turns/osm.py
@@ -1,6 +1,10 @@
 import coords, geom, graph
+import sys
+from collections import deque
 
+import pandas as pd
 from imposm.parser import OSMParser
+import numpy as np
 
 BLACKLIST = set([
 	"pedestrian",
@@ -21,8 +25,14 @@ BLACKLIST = set([
 ])
 
 def read(fname, bb=None, ll_bb=None):
-	g = graph.Graph()
-	vertex_map = {}
+
+        vertex_set = set([])
+        vertices = deque([]) # (osm_id, lon, lat)
+
+        used_vertices = set([])
+        out_ways = deque([])
+        edges = deque([]) # (osm_id, index, src, dst)
+        
 	def coords_callback(clist):
 		for osmid, lon, lat in clist:
 			p = geom.FPoint(lon, lat)
@@ -31,7 +41,10 @@ def read(fname, bb=None, ll_bb=None):
 			p = coords.lonLatToMeters(p).to_point()
 			if bb is not None and not bb.contains(p):
 				continue
-			vertex_map[osmid] = g.add_vertex(p)
+
+                        vertices.append((osmid, np.double(lon), np.double(lat)))
+                        vertex_set.add(osmid)
+                        
 	def ways_callback(ways):
 		for osmid, tags, refs in ways:
 			if 'highway' not in tags or tags['highway'] in BLACKLIST:
@@ -39,11 +52,38 @@ def read(fname, bb=None, ll_bb=None):
 			for i in range(len(refs) - 1):
 				src = refs[i]
 				dst = refs[i + 1]
-				if src not in vertex_map or dst not in vertex_map:
+				if src not in vertex_set or dst not in vertex_set:
 					continue
-				g.add_bidirectional_edge(vertex_map[src], vertex_map[dst])
+                                edges.append((osmid, i, src, dst))
+                                used_vertices.add(src)
+                                used_vertices.add(dst)
+
+                        out_ways.append((osmid, refs))
+
 	p = OSMParser(coords_callback=coords_callback)
 	p.parse(fname)
 	p = OSMParser(ways_callback=ways_callback)
-	p.parse(fname)
-	return g
+        p.parse(fname)
+
+        final_vertices = deque([])
+        for v in vertices:
+             (osm_id, _, _) = v
+             if osm_id in used_vertices:
+                     final_vertices.append(v)
+
+        return final_vertices, edges, out_ways
+
+
+if __name__ == '__main__':
+        import json
+        (v, e, w) = read(sys.argv[1], bb=None, ll_bb=None)
+        # vtx = pd.DataFrame(data=list(v), columns=['osm_id', 'lon', 'lat'])
+        # vtx.to_csv('osm_vertices_ma2.csv', index=False)
+        # edg = pd.DataFrame(data=list(e), columns=['osm_id', 'index', 'src', 'dst'])
+        # edg.to_csv('osm_edges_ma2.csv', index=False)
+        ways = pd.DataFrame(data=list(w), columns=['osm_id', 'refs'])
+        ways['refs'] = ways.refs.map(json.dumps)
+        ways.to_csv('osm_ways_ma2.csv', index=False)
+        
+        
+        
